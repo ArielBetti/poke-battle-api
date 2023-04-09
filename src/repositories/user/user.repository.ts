9981df50aef89a8951eaa2be5bf8f6e5bfcbff24
ArielBetti <ariel.betti@gmail.com>
2023-04-09 19:51:34 -0300

    feat: update user repository

--- src/repositories/user/user.repository.ts
+++ src/repositories/user/user.repository.ts
@@ -1,33 +1,53 @@
 import { provide } from "inversify-binding-decorators";
-import { prismaClient } from "database/prismaClient";
+import bcrypt from "bcrypt";
+import { prismaClient } from "@providers/database/prismaClient";
 import { IUserRepository } from "./user-repository.interface";
 import { IUserDTO } from "./user.dto";
+import { User } from "@entities/user.entity";
+import { mapToPrisma } from "@providers/helpers/mapToPrisma";
+import excludeFromQuery from "@providers/helpers/excludeFromQuery";
 
 @provide(UserRepository)
 class UserRepository implements IUserRepository {
     private readonly repository = prismaClient;
 
-    async find(id: string): Promise<IUserDTO | null> {
+    async find(id: string): Promise<{ name: string; email: string } | null> {
         const user = await this.repository.user.findUnique({ where: { id } });
+
+        return user ? this.mapToDTOWithoutPassword(user) : null;
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        const user = await this.repository.user.findUnique({
+            where: { email },
+        });
         return user ? this.mapToDTO(user) : null;
     }
 
-    async findAll(): Promise<IUserDTO[]> {
+    async findAll(): Promise<{ name: string; email: string }[]> {
         const users = await this.repository.user.findMany();
-        return users.map(this.mapToDTO);
+
+        return users.map((user) => this.mapToFindAllDTO(user));
     }
 
-    async create(user: IUserDTO): Promise<IUserDTO> {
-        const createdUser = await this.repository.user.create({ data: user });
+    async create(user: IUserDTO): Promise<User> {
+        const password = await bcrypt.hash(user.password, 10);
+        const createdUser = await this.repository.user.create({
+            data: mapToPrisma({
+                ...user,
+                password,
+            }),
+        });
         return this.mapToDTO(createdUser);
     }
 
-    async update(id: string, user: IUserDTO): Promise<IUserDTO | null> {
+    async update(id: string, user: IUserDTO): Promise<User | null> {
         const updatedUser = await this.repository.user.update({
             where: { id },
             data: user,
         });
-        return updatedUser ? this.mapToDTO(updatedUser) : null;
+
+        return this.mapToDTO(updatedUser);
     }
 
     async delete(id: string): Promise<boolean> {
@@ -35,8 +55,30 @@ class UserRepository implements IUserRepository {
         return true;
     }
 
-    private mapToDTO(user: any): IUserDTO {
-        return { id: user.id, email: user.email, name: user.name };
+    private mapToDTO(user: IUserDTO): User {
+        const newUser = new User(user.name, user.email, user.password, user.id);
+        return newUser;
+    }
+
+    private mapToDTOWithoutPassword(user: IUserDTO): {
+        name: string;
+        email: string;
+        id: string;
+    } {
+        const newUser = new User(user.name, user.email, user.password, user.id);
+        return {
+            name: newUser.name,
+            email: newUser.email,
+            id: newUser.id,
+        };
+    }
+
+    private mapToFindAllDTO(user: IUserDTO): { name: string; email: string } {
+        const newUser = new User(user.name, user.email, user.password, user.id);
+        return {
+            name: newUser.name,
+            email: newUser.email,
+        };
     }
 }
 
