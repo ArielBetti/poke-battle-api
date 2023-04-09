82ed4e50761a68b5f96c55bf6b338fe5efe8419a
ArielBetti <ariel.betti@gmail.com>
2023-04-09 19:48:11 -0300

    feat: add new model to user

--- prisma/migrations/20230409174438_init/migration.sql
+++ prisma/migrations/20230409174438_init/migration.sql
@@ -2,7 +2,8 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
